4c0cd870177f085ffe0b58b7d607d5e2b978a3c5
InvisiblePotion <dkagmrfbxks@gmail.com>
2023-04-10 15:31:50 +0900

    230410: 스프링 초기화 폴더 업로드 시도 1차

---
+++ LOL_Project/src/main/java/com/base/personlol/dao/Example_dao.java
@@ -0,0 +1,11 @@
+package com.base.personlol.dao;
+
+import java.util.Map;
+
+import com.base.personlol.dto.Example_dto;
+
+public interface Example_dao {
+	
+	Example_dto testDB(Map<String, Integer> nums);
+	
+}
