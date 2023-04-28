931b998c2d28f9637040e80c4f0a0fd66fc17f6f
JuChan-Lee <20090839@vision.hoseo.edu>
2023-04-28 13:49:46 +0900

    Merge branch 'workspace_Moon' of https://github.com/InvisiblePotion/lolproject-workspace
    # Conflicts:
    #	Personlol_Project/src/main/java/com/base/personlol/controller/Example_Controller.java
    #	Personlol_Project/src/main/resources/mapper/Example_mapper.xml
    #	Personlol_Project/src/main/resources/mybatis-conf.xml
    #	Personlol_Project/src/main/webapp/WEB-INF/views/home.jsp

---
+++ Personlol_Project/src/main/java/com/base/personlol/dto/Test_member_dto.java
@@ -0,0 +1,17 @@
+package com.base.personlol.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+
+@Alias("member")
+public class Test_member_dto {
+	private String userid;
+	private String userpw;
+	private String pwchk;
+	private String account;
+}
