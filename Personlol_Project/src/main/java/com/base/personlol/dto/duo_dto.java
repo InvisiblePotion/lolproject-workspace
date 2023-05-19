3709a64b557b0d8257f4df198bba9572d1a68b39
miira6010 <miira6010@gmail.com>
2023-05-19 11:37:59 +0900

    0519 듀오 커밋

--- Personlol_Project/src/main/java/com/base/personlol/dto/duo_dto.java
+++ Personlol_Project/src/main/java/com/base/personlol/dto/duo_dto.java
@@ -1,10 +1,22 @@
 package com.base.personlol.dto;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
 import lombok.Data;
-import lombok.experimental.Accessors;
 
 @Data
-@Accessors(chain = true)
+@Alias("duo")
 public class duo_dto {
-
+	
+	private Long duo_id;
+	private String duo_ownerid;
+	private String duo_accepter;
+	private String duo_content;
+	private Long duo_active;
+	private Timestamp duo_date;
+	private String duo_title;	
 }
+
